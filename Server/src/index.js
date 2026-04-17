3403d05cff5b9def0e5d500508cfa008709fae6f
ishaan khandelwal <ishaankhandelwal2007@gmail.com>
2026-04-17 20:40:47 +0530

    feat: initialize express server with auth routes and add vercel deployment configuration

--- Server/src/index.js
+++ Server/src/index.js
@@ -2,15 +2,21 @@ const express = require("express");
 const app = express()
 const connectDB = require("./connection")
 const authRoutes = require("./routes/auth.routes")
-const dotenv = require("dotenv")
-const { authenticateToken } = require("../middleware/auth")
+// const dotenv = require("dotenv")
+const authenticateToken = require("../middleware/auth")
 
-dotenv.config()
+// dotenv.config()
 
 connectDB()
 
 app.use(express.json())
-app.use('/api', authenticateToken, authRoutes)
+app.use('/api', authRoutes)
+app.get('/api/verify-token', authenticateToken, (req, res) => {
+    res.status(200).json({
+        message: "Token is valid",
+        userId: req.userId
+    })
+})
 
 app.listen(65535, () => {
     console.log(`Server running on port http://localhost:65535`);
